278b0fcf0c0d433a12c7b474a165b4dc36fa85b6
214X <92593070+214X@users.noreply.github.com>
2026-04-11 21:40:27 +0300

    Added a redirection for /kopyası-latso URL.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   // cssModules: {
   //   mode: "local",
   // },
+  async redirects() {
+    return [
+      {
+        source: '/en/kopyası-latso',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
